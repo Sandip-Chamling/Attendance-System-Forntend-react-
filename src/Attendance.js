be7c061895899bb9866c7dfd13a5b2a97de63396
Sandip-Chamling <chamlingsandip2000@gmail.com>
2024-09-10 11:30:30 +0545

    Fixed layout issue in Attendance

--- src/Attendance.js
+++ src/Attendance.js
@@ -1,4 +1,4 @@
-// src/components/attendance.js
+
 import React, { useState, useEffect } from 'react';
 import { getFromLocalStorage, saveToLocalStorage } from './components/LocalStorageUtils';
 import Swal from 'sweetalert2';
